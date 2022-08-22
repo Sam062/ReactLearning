866af5b804b393cf2aeaeb95ee07c152e8399402
sam062 <ip6.sam@gmail.com>
2022-08-22 17:56:09 +0530

    added event binding and data access from element

---
+++ EventBindingsAndDataAccessFromElements/src/App.js
@@ -0,0 +1,71 @@
+class ProductComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            product:{
+                name: '',
+                price: ''
+            }
+        }
+        this.nameChanged=this.nameChanged.bind(this);
+        this.priceChanged=this.priceChanged.bind(this);
+    }
+    
+    nameChanged(event){
+        this.setState({
+            product: {
+                name: event.target.value,
+                price: this.state.product.price
+            }
+        })
+    }
+    priceChanged(event){
+        this.setState({
+            product: {
+                name: this.state.product.name,
+                price: event.target.value
+            }
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <div>
+                    <h3>Register Product</h3>
+                    <dl>
+                        <dt>Product Name</dt>
+                        <dd><input type="text" onChange={this.nameChanged} /></dd>
+                        <dt>Product Price</dt>
+                        <dd><input type="text" onChange={this.priceChanged} /></dd>
+                    </dl>
+                    <h3>Product Details</h3>
+                    <dl>
+                        <dt>Name</dt>
+                        <dd>{this.state.product.name}</dd>
+                        <dt>Price</dt>
+                        <dd>{this.state.product.price}</dd>
+                    </dl>
+                </div>
+            </>
+        )
+    }
+}
+class MainComponent extends React.Component {
+    render() {
+        return (
+            <>
+                <ProductComponent />
+            </>
+        )
+    }
+}
+
+
+
+ReactDOM.render(
+    <>
+        <MainComponent />
+    </>,
+    document.getElementById("container")
+)
