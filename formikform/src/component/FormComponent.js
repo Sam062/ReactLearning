268c30b18f74b8ea831d5e454ec791025ea07903
sam062 <ip6.sam@gmail.com>
2022-08-24 10:37:48 +0530

    fixed checkbox isssue

--- formikform/src/component/FormComponent.js
+++ formikform/src/component/FormComponent.js
@@ -6,7 +6,8 @@ const FormComponent = () => {
         initialValues: {
             productId: "",
             Name: "",
-            Price: ""
+            Price: 0,
+            inStock: false
         },
         onSubmit: values => {
             document.write(JSON.stringify(values));
@@ -21,6 +22,8 @@ const FormComponent = () => {
                 productId : <input type="text" name="productId" onChange={formik.handleChange} value={formik.values.productId} /> <br />
                 Name : <input type="text" name="Name" onChange={formik.handleChange} value={formik.values.Name} /><br />
                 Price : <input type="text" name="Price" onChange={formik.handleChange} value={formik.values.Price} /><br />
+                In Stock: <input type="checkbox" name="inStock" onChange={formik.handleChange} checked={formik.values.inStock} />
+
                 <button>Register</button>
 
 
