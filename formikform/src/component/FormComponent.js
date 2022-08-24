8b3945e857b9609872cfd2f609fe24d18d7fd7e4
sam062 <ip6.sam@gmail.com>
2022-08-24 11:13:37 +0530

    added bootstrap

--- formikform/src/component/FormComponent.js
+++ formikform/src/component/FormComponent.js
@@ -10,25 +10,49 @@ const FormComponent = () => {
             inStock: false
         },
         onSubmit: values => {
-            document.write(JSON.stringify(values));
+            document.write(`
+                <h2>Product Details</h2>
+                <table className="table">
+                    <tr>
+                        <td>Name</td>
+                        <td>${values.Name}</td>
+                    </tr>
+                    <tr>
+                        <td>Product-Id</td>
+                        <td>${values.productId}</td>
+                    </tr>
+                    <tr>
+                        <td>Price</td>
+                        <td>${values.Price}</td>
+                    </tr>
+                    <tr>
+                        <td>In Stock</td>
+                        <td>${(values.inStock==true)?"Available":"Out Of Stock"}</td>
+                    </tr>
+                </table>
+            `);
         }
     })
     return (
         <>
-            <form onSubmit={formik.handleSubmit}>
-                <h1>
-                    Register Product
-                </h1>
-                productId : <input type="text" name="productId" onChange={formik.handleChange} value={formik.values.productId} /> <br />
-                Name : <input type="text" name="Name" onChange={formik.handleChange} value={formik.values.Name} /><br />
-                Price : <input type="text" name="Price" onChange={formik.handleChange} value={formik.values.Price} /><br />
-                In Stock: <input type="checkbox" name="inStock" onChange={formik.handleChange} checked={formik.values.inStock} />
-
-                <button>Register</button>
-
-
-
-            </form>
+            <div className="container">
+                <form onSubmit={formik.handleSubmit}>
+                    <h1>Register Product</h1>
+                    <dl>
+                        <dt>productId</dt>
+                        <dd><input className="form-control" type="text" name="productId" onChange={formik.handleChange} value={formik.values.productId} /> </dd>
+                        <dt>Name</dt>
+                        <dd><input className="form-control" type="text" name="Name" onChange={formik.handleChange} value={formik.values.Name} /> </dd>
+                        <dt>Price</dt>
+                        <dd><input className="form-control" type="text" name="Price" onChange={formik.handleChange} value={formik.values.Price} /> </dd>
+                        <dt>InStock</dt>
+                        <dd className="form-check form-switch"><input className="form-check-input" type="checkbox" name="inStock" onChange={formik.handleChange} checked={formik.values.inStock} /> </dd>
+                    </dl>
+                    <div className="d-grid">
+                    <button className="btn btn-success">Register</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
